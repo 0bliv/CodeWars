931e86b71d599326c95c0e01236a31e23d255c14
0bliv <88675619+0bliv@users.noreply.github.com>
2023-11-08 21:04:44 -0300

    Solved

---
+++ 6kyu/Two Sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    num_index_map = {}
+    
+    for i, num in enumerate(nums):
+        complement = target - num
+        
+        if complement in num_index_map:
+            return (num_index_map[complement], i)
+        
+        num_index_map[num] = i
+    
+    return None
---
+++ 7kyu/Flatten and sort an array.py
@@ -0,0 +1,2 @@
+def flatten_and_sort(array):
+    return sorted([num for sub in array for num in sub])
